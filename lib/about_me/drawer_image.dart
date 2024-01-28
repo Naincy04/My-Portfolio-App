38ee5fbe7a1e818003a7e81fd96bee1da079d939
Naincy04 <naincy2102@gmail.com>
2024-01-28 17:31:49 +0530

    Project section

--- lib/about_me/drawer_image.dart
+++ lib/about_me/drawer_image.dart
@@ -8,11 +8,11 @@ class DrawerImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
+      height: 300,
+      width: 300,
       padding: const EdgeInsets.all(defaultPadding / 6),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
+          borderRadius: BorderRadius.circular(150),
           gradient: LinearGradient(
             colors: [
               Colors.pink,
@@ -25,11 +25,11 @@ class DrawerImage extends StatelessWidget {
                 color: Colors.blue, blurRadius: 10, offset: Offset(0, -2)),
           ]),
       child: ClipRRect(
-          borderRadius: BorderRadius.circular(50),
+          borderRadius: BorderRadius.circular(150),
           child: Transform.rotate(
               angle: 0.1,
               child: Image.asset(
-                'assets/images/profile.png',
+                'assets/images/profile.jpg',
                 fit: BoxFit.cover,
               ))),
     );
--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -8,50 +8,26 @@ class Project {
 
 List<Project> projectList = [
   Project(
-    'Coffee Application',
-    'Presenting "Coffee" - a Flutter UI application that invites you to indulge in a delightful coffee adventure. Immerse yourself in the world of rich aroma and flavor as you browse through an exquisite selection of specialty coffees. With a user-friendly interface, exploring different blends and discovering their unique details becomes a delightful experience.',
+    'Dice roller app',
+    'The Dice Roller app in Flutter provides users with a simple and engaging way to simulate rolling a six-sided die. The app features an intuitive user interface, displaying a central area where users can see the current rolled dice face. Users can initiate the dice roll by tapping a dedicated button, triggering a random number generation between 1 and 6, simulating the roll of a physical die.',
     'assets/images/coffee.png',
-    'https://github.com/Hamad-Anwar/Coffe-Shop-Beautifull-UI',
+    'https://github.com/Naincy04/dice-roller-app',
   ),
   Project(
-    'Car Controller Application UI',
-    'Car Control Dashboard is a user-friendly mobile application built using Flutter and powered by GetX for efficient state management. Enjoy a modern and intuitive design that adapts to various screen sizes, Interact with dynamic car controls, including speed, steering, and temperature. Realistic animations enhance the visual appeal, making it feel like you are controlling a real car!',
+    'Quiz App',
+    'The Quiz App in Flutter offers an interactive and educational platform for users to engage with a variety of quizzes on diverse topics. The app boasts a user-friendly interface, providing an enjoyable experience for both casual learners and quiz enthusiasts. Users can choose from a range of quiz categories, answer questions, and receive instant feedback on their performance. With features such as scoring, time tracking, and progress tracking, the app encourages users to enhance their knowledge and challenge themselves. The visually appealing design, coupled with smooth animations and transitions, ensures an immersive and enjoyable quiz-taking experience. Whether used for self-assessment, learning new facts, or competitive play, the Flutter Quiz App is a versatile and engaging tool for knowledge exploration.',
     'assets/images/car.png',
-    'https://github.com/Hamad-Anwar/Car-Controller-Getx-Flutter',
+    'https://github.com/Naincy04/quiz-app',
   ),
   Project(
-      'Neumorphic Clockify',
-      'Neumorphic TimeKit is an open-source project that brings together the elegance of a beautifully designed alarm system, an analog clock with a neumorphic touch, and a feature-rich stopwatch. Whether you\'re looking for a functional alarm tool, a stylish desktop clock, or a precise stopwatch, TimeKit has you covered.',
-      'assets/images/alarm.jpg',
-      'https://github.com/Hamad-Anwar/Neumorphic-Analog-Clockify'),
+    'To do app',
+    'The To-Do App in Flutter is a powerful and intuitive task management tool designed to streamline users daily productivity. With a clean and user-friendly interface, the app enables users to create, organize, and prioritize tasks effortlessly. Users can add tasks, set due dates, and categorize items for easy identification. The app provides features like task completion tracking, allowing users to mark items as done, and intuitive gestures for efficient task management. With real-time syncing across devices, users can access their to-do lists seamlessly from anywhere. The Flutter To-Do App aims to enhance productivity and organization, making it an essential companion for individuals seeking an efficient way to manage their daily tasks and responsibilities',
+    'assets/images/alarm.jpg',
+    'https://github.com/Naincy04/to-do-app',
+  ),
   Project(
-      'CUI Aider',
-      'CUI AIDER is an extraordinary application that brings together augmented reality (AR), a robust student portal, intuitive class management, and a comprehensive GPA system, providing an all-encompassing solution for students.Using AR technology, CUI AIDER offers an immersive experience, allowing users to explore the complete university campus virtually. Through AR, students can visualize buildings, navigate with interactive directions, and gain a better understanding of their surroundings.',
+      'Compliment Generator',
+      'The Compliment Generator Task in Flutter is a delightful and uplifting application designed to spread positivity and brighten users days. With a charming and user-friendly interface, the app generates personalized compliments with just a tap. Users can enjoy a variety of compliments, ranging from encouraging messages to light-hearted affirmations. The application not only aims to boost users spirits but also provides an interactive and enjoyable experience. Whether used for self-motivation or to share compliments with friends, the Flutter Compliment Generator Task is a fun and heartwarming addition to daily routines, fostering a positive and affirming atmosphere.',
       'assets/images/cui.png',
-      'https://github.com/Hamad-Anwar/'),
-  Project(
-      'Flutter Music Streaming Application',
-      'A Flutter music streaming application that allows users to play audio files from local storage. The app features a beautiful neumorphic UI design and provides a smooth user experience for browsing and playing music.',
-      'assets/images/player.png',
-      'https://github.com/Hamad-Anwar/Neumorphic-Music_Player-Flutter'),
-  Project(
-      'Food Recipe App',
-      'This repository contains a Flutter implementation of a Food Recipe app with a captivating introduction section and impressive animations. It also features a signup and signin page with animations, elegantly presented on a bottom sheet.',
-      'assets/images/recipe.png',
-      'https://github.com/Hamad-Anwar/Food-Recipe-App-Flutter'),
-  Project(
-      'Task Sync Pro',
-      'Welcome to the Beautiful Task Scheduler App repository! This Flutter-based task management application combines elegant design with a robust backend, ensuring a seamless and organized task management experience. From stunning UI to real-time synchronization, this app has you covered.',
-      'assets/images/task.png',
-      'https://github.com/Hamad-Anwar/Task-Sync-Pro-Flutter'),
-  Project(
-      'Flutter Chat Application with Firebase',
-      'Welcome to our innovative Flutter chat application! This feature-rich messaging platform allows users to connect and communicate seamlessly through text and images. The app is built using Flutter for the frontend and integrates with Firebase for backend services, including authentication, real-time database, and storage.',
-      'assets/images/chat.png',
-      'https://github.com/Hamad-Anwar/Messenger-App-Backend-Firebase'),
-  Project(
-      'Doctor Appointment Application',
-      'Introducing the extraordinary "Doctor Appointment System" - a state-of-the-art Flutter UI application that redefines healthcare accessibility and efficiency. Seamlessly crafted, this app empowers users to effortlessly select doctors based on categories, engage in smooth messaging, and access detailed profiles.',
-      'assets/images/doctor.png',
-      'https://github.com/Hamad-Anwar/Doctor-Appointment-Application-UI'),
+      'https://github.com/Naincy04/compliment-generator-task'),
 ];
